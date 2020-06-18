e019915300d820d8060c88cd188375999703ac11
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2020-06-18 20:10:28 +0530

    new: cache the changes and dump all at once Now, instead of manipulating the pointer after each change, the transpiler stores the changes, and flushes the changes out all at once before sensitive instructions, such as loop in/out, reset and etc. This improves performance quite a bit, the gcc -03 build of mandelbrot completing consistently ~1 second now. But, the clang build is going into an infinite loop with anything after -O0, and I have no clue why.

--- Makefile
+++ Makefile
@@ -1,5 +1,13 @@
+override CFLAGS += -Wall -Wextra
+
 bfvm: bfvm.c
 	$(CC) bfvm.c -O3 -Wall -Wextra -o bfvm
+
+debug: bfvm.c
+	$(CC) bfvm.c -O0 -g3 -Wall -Wextra -DDEBUG -o bfvm
+
+mandeloptcheck: mandelbrot.c
+	$(CC) mandelbrot.c -O0 -g3 $(CFLAGS) -o mandel
 
 profile: bfvm.c
 	$(CC) bfvm.c -O2 -Wall -Wextra -g3 -o bfvm
--- bfvm.c
+++ bfvm.c
@@ -26,30 +26,45 @@ enum {
 };
 
 typedef struct {
-	int *values, size, capacity;
-} Array;
+	int idx;
+	int value;
+} Change;
 
-void array_init(Array *a) {
-	a->capacity = a->size = 0;
-	a->values             = NULL;
-}
-
-void array_insert(Array *a, int value) {
-	if(a->size == a->capacity) {
-		if(a->capacity == 0)
-			a->capacity = 1;
-		else
-			a->capacity *= 2;
-		a->values = (int *)realloc(a->values, sizeof(int) * a->capacity);
+#define ARRAY(type, name)                                                  \
+	typedef struct {                                                       \
+		type *values;                                                      \
+		int   size, capacity;                                              \
+	} name##Array;                                                         \
+                                                                           \
+	void type##_array_init(name##Array *a) {                               \
+		a->capacity = a->size = 0;                                         \
+		a->values             = NULL;                                      \
+	}                                                                      \
+                                                                           \
+	void type##_array_resize(name##Array *a, int capacity) {               \
+		a->values   = (type *)realloc(a->values, sizeof(type) * capacity); \
+		a->capacity = capacity;                                            \
+	}                                                                      \
+                                                                           \
+	void type##_array_insert(name##Array *a, type value) {                 \
+		if(a->size == a->capacity) {                                       \
+			if(a->capacity == 0)                                           \
+				a->capacity = 1;                                           \
+			else                                                           \
+				a->capacity *= 2;                                          \
+			type##_array_resize(a, a->capacity);                           \
+		}                                                                  \
+		a->values[a->size++] = value;                                      \
+	}                                                                      \
+                                                                           \
+	void type##_array_free(name##Array *a) {                               \
+		free(a->values);                                                   \
+		a->capacity = a->size = 0;                                         \
+		a->values             = NULL;                                      \
 	}
-	a->values[a->size++] = value;
-}
 
-void array_free(Array *a) {
-	free(a->values);
-	a->capacity = a->size = 0;
-	a->values             = NULL;
-}
+ARRAY(int, Int);
+ARRAY(Change, Change);
 
 bool skipAll(char **s) {
 	char *source  = *s;
@@ -74,7 +89,7 @@ bool skipAll(char **s) {
 	return skipped;
 }
 
-void check_repeat(char c, char **source, Array *program, int code_start) {
+void check_repeat(char c, char **source, IntArray *program, int code_start) {
 	int   count = 0; // 0 based count, as c has already occurred
 	char *s     = *source;
 	while(*s == c || (skipAll(&s) && *s == c)) {
@@ -90,48 +105,48 @@ void check_repeat(char c, char **source, Array *program, int code_start) {
 		case 4:
 		case 5:
 		case 6:
-		case 7: array_insert(program, code_start + count); break;
+		case 7: int_array_insert(program, code_start + count); break;
 		default:
-			array_insert(program, code_start + 8);
-			array_insert(program, count + 1);
+			int_array_insert(program, code_start + 8);
+			int_array_insert(program, count + 1);
 			break;
 	}
 }
 
-Array *compile(char *source) {
-	Array *program = (Array *)malloc(sizeof(Array));
-	array_init(program);
-	Array jumpstack;
-	array_init(&jumpstack);
+IntArray *compile(char *source) {
+	IntArray *program = (IntArray *)malloc(sizeof(IntArray));
+	int_array_init(program);
+	IntArray jumpstack;
+	int_array_init(&jumpstack);
 	while(*source) {
 		switch(*source++) {
 			case '>': check_repeat('>', &source, program, RIGHT_1); break;
 			case '<': check_repeat('<', &source, program, LEFT_1); break;
 			case '+': check_repeat('+', &source, program, INCR_1); break;
 			case '-': check_repeat('-', &source, program, DECR_1); break;
-			case '.': array_insert(program, OUTPUT); break;
-			case ',': array_insert(program, INPUT); break;
+			case '.': int_array_insert(program, OUTPUT); break;
+			case ',': int_array_insert(program, INPUT); break;
 			case '[':
 				if(*source == '-' && *(source + 1) == ']') {
 					source++; // -
 					source++; // ]
-					array_insert(program, RESET_CELL);
+					int_array_insert(program, RESET_CELL);
 				} else {
-					array_insert(program, JMPZ);
-					array_insert(program, 0);
-					array_insert(&jumpstack, program->size);
+					int_array_insert(program, JMPZ);
+					int_array_insert(program, 0);
+					int_array_insert(&jumpstack, program->size);
 				}
 				break;
 			case ']': {
 				if(jumpstack.size == 0) {
 					printf("Unmatched ']'!\n");
-					array_free(program);
-					array_free(&jumpstack);
+					int_array_free(program);
+					int_array_free(&jumpstack);
 					return NULL;
 				}
 				int lastJump = jumpstack.values[--jumpstack.size];
-				array_insert(program, JMPNZ);
-				array_insert(program, lastJump - program->size - 1);
+				int_array_insert(program, JMPNZ);
+				int_array_insert(program, lastJump - program->size - 1);
 				program->values[lastJump - 1] = program->size - lastJump;
 				break;
 			}
@@ -140,16 +155,64 @@ Array *compile(char *source) {
 	}
 	if(jumpstack.size > 0) {
 		printf("Unmatched '['!\n");
-		array_free(program);
-		array_free(&jumpstack);
+		int_array_free(program);
+		int_array_free(&jumpstack);
 		return NULL;
 	}
-	array_free(&jumpstack);
-	array_insert(program, END);
+	int_array_free(&jumpstack);
+	int_array_insert(program, END);
 	return program;
 }
 
+#define SPECIALIZED8_SINGLE_LINE_DEBUG(name, num) \
+	case name##_##num:                            \
+		printf(#name "\t\t" #num);                \
+		break;
+#define SPECIALIZED8_LINE_X_DEBUG(name)           \
+	case name##_X:                                \
+		printf(#name "_X\t\t%d", *(program + 1)); \
+		return ip + 2;
+#define SPECIALIZED8_DEBUG(name)             \
+	SPECIALIZED8_SINGLE_LINE_DEBUG(name, 1); \
+	SPECIALIZED8_SINGLE_LINE_DEBUG(name, 2); \
+	SPECIALIZED8_SINGLE_LINE_DEBUG(name, 3); \
+	SPECIALIZED8_SINGLE_LINE_DEBUG(name, 4); \
+	SPECIALIZED8_SINGLE_LINE_DEBUG(name, 5); \
+	SPECIALIZED8_SINGLE_LINE_DEBUG(name, 6); \
+	SPECIALIZED8_SINGLE_LINE_DEBUG(name, 7); \
+	SPECIALIZED8_SINGLE_LINE_DEBUG(name, 8); \
+	SPECIALIZED8_LINE_X_DEBUG(name);
+
+int disassemble_single(int ip, int *program) {
+	printf("%6d: ", ip);
+	switch(*program) {
+		SPECIALIZED8_DEBUG(INCR);
+		SPECIALIZED8_DEBUG(DECR);
+		SPECIALIZED8_DEBUG(LEFT);
+		SPECIALIZED8_DEBUG(RIGHT);
+		case INPUT: printf("INPUT"); break;
+		case OUTPUT: printf("OUTPUT"); break;
+		case JMPZ: printf("JMPZ\t\t%d", *(program + 1)); return ip + 2;
+		case JMPNZ: printf("JMPNZ\t\t%d", *(program + 1)); return ip + 2;
+		case RESET_CELL: printf("RESET_CELL"); break;
+		case END: printf("END"); break;
+		case START: printf("START"); break;
+	}
+	return ip + 1;
+}
+
+void disassemble_all(IntArray *pgm) {
+	int *program = pgm->values;
+	for(int i = 0; i < pgm->size;) {
+		i = disassemble_single(i, &program[i]);
+		printf("\n");
+	}
+	printf("\n");
+}
+
+#ifndef DEBUG
 #define BFVM_COMPUTED_GOTO
+#endif
 
 #define next_code() (*(code++))
 #ifdef BFVM_COMPUTED_GOTO
@@ -186,7 +249,7 @@ Array *compile(char *source) {
 	SPECIALIZED8_SINGLE_INS(name, x, op, 8); \
 	SPECIALIZED8_INS_X(name, x, op);
 
-void execute(Array *program) {
+void execute(IntArray *program) {
 	int *    code = program->values;
 	uint8_t *cell = memory;
 #ifdef BFVM_COMPUTED_GOTO
@@ -210,6 +273,10 @@ void execute(Array *program) {
 	                         &&LABEL_END};
 #endif
 	LOOP() {
+#ifdef DEBUG
+		disassemble_single(code - program->values, code);
+		printf("\t\tCell: %ld\t\tValue: %u\n", cell - memory, *cell);
+#endif
 		SWITCH() {
 			SPECIALIZED8_IMPL(INCR, (*cell), +);
 			SPECIALIZED8_IMPL(DECR, (*cell), -);
@@ -249,18 +316,85 @@ void execute(Array *program) {
 	}
 }
 
+void print_indent(FILE *f, int level) {
+	for(int i = 0; i < level; i++) fprintf(f, "\t");
+}
+
+void dump_changes(FILE *f, ChangeArray *totalChange, int pointerShift,
+                  int level) {
+	// dump the changes
+	for(int i = 0; i < totalChange->size; i++) {
+		print_indent(f, level);
+		fprintf(f, "cell[%d] += %d;\n", totalChange->values[i].idx,
+		        totalChange->values[i].value);
+#ifdef DEBUG
+		// print the value of the cell
+		fprintf(f, "#ifdef DEBUG\n");
+		print_indent(f, level);
+		fprintf(
+		    f,
+		    "printf(\"\t\tCell: %%ld\\t\\tValue: %%u\\n\", cell + %d - memory, "
+		    "cell[%d]);\n",
+		    totalChange->values[i].idx, totalChange->values[i].idx);
+		fprintf(f, "#endif\n");
+#endif
+	}
+	if(pointerShift != 0) {
+		print_indent(f, level);
+		fprintf(f, "cell += %d;\n", pointerShift);
+	}
+#ifdef DEBUG
+	if(pointerShift != 0 || totalChange->size == 0) {
+		// print the value of the cell
+		fprintf(f, "#ifdef DEBUG\n");
+		print_indent(f, level);
+		fprintf(f,
+		        "printf(\"\t\tCell: %%ld\\t\\tValue: %%u\\n\", cell - memory, "
+		        "*cell);\n");
+		fprintf(f, "#endif\n");
+	}
+#endif
+	// release the array
+	Change_array_free(totalChange);
+}
+
+void apply_total_change(ChangeArray *ca, int currentPointer, int totalChange) {
+	for(int i = 0; i < ca->size; i++) {
+		if(ca->values[i].idx == currentPointer) {
+			ca->values[i].value += totalChange;
+			return;
+		}
+	}
+	Change_array_insert(ca, (Change){currentPointer, totalChange});
+}
+
+#define SPECIALIZED8_SINGLE_LINE2(name, x, op, num)                   \
+	case name##_##num:                                                \
+		apply_total_change(x, currentPointer - startPointer, op num); \
+		break;
+#define SPECIALIZED8_LINE_X2(name, x, op)                    \
+	case name##_X:                                           \
+		apply_total_change(x, currentPointer - startPointer, \
+		                   op * (program++));                \
+		break;
+#define SPECIALIZED8_LINE2(name, x, op)        \
+	SPECIALIZED8_SINGLE_LINE2(name, x, op, 1); \
+	SPECIALIZED8_SINGLE_LINE2(name, x, op, 2); \
+	SPECIALIZED8_SINGLE_LINE2(name, x, op, 3); \
+	SPECIALIZED8_SINGLE_LINE2(name, x, op, 4); \
+	SPECIALIZED8_SINGLE_LINE2(name, x, op, 5); \
+	SPECIALIZED8_SINGLE_LINE2(name, x, op, 6); \
+	SPECIALIZED8_SINGLE_LINE2(name, x, op, 7); \
+	SPECIALIZED8_SINGLE_LINE2(name, x, op, 8); \
+	SPECIALIZED8_LINE_X2(name, x, op);
+
 #define SPECIALIZED8_SINGLE_LINE(name, x, op, num) \
 	case name##_##num:                             \
-		fprintf(f, #x " " #op "="                  \
-		              " " #num ";\n");             \
+		x op## = num;                              \
 		break;
 #define SPECIALIZED8_LINE_X(name, x, op) \
 	case name##_X:                       \
-		fprintf(f,                       \
-		        #x " " #op "="           \
-		           " "                   \
-		           "%d;\n",              \
-		        *program++);             \
+		x op## = *program++;             \
 		break;
 #define SPECIALIZED8_LINE(name, x, op)        \
 	SPECIALIZED8_SINGLE_LINE(name, x, op, 1); \
@@ -273,38 +407,127 @@ void execute(Array *program) {
 	SPECIALIZED8_SINGLE_LINE(name, x, op, 8); \
 	SPECIALIZED8_LINE_X(name, x, op);
 
-void transpile_rec(FILE *f, int **pgm, int level) {
+#ifdef DEBUG
+#define SPECIALIZED8_SINGLE_LINE_PRINTF(name, num)            \
+	case name##_##num:                                        \
+		fprintf(f, "printf(\"" #name "\\t\\t" #num "\");\n"); \
+		break;
+#define SPECIALIZED8_LINE_X_PRINTF(name)                                       \
+	case name##_X:                                                             \
+		fprintf(f, "printf(\"" #name "_X\\t\\t%%d\", %d);\n", *(program + 1)); \
+		break;
+#define SPECIALIZED8_PRINTF(name)             \
+	SPECIALIZED8_SINGLE_LINE_PRINTF(name, 1); \
+	SPECIALIZED8_SINGLE_LINE_PRINTF(name, 2); \
+	SPECIALIZED8_SINGLE_LINE_PRINTF(name, 3); \
+	SPECIALIZED8_SINGLE_LINE_PRINTF(name, 4); \
+	SPECIALIZED8_SINGLE_LINE_PRINTF(name, 5); \
+	SPECIALIZED8_SINGLE_LINE_PRINTF(name, 6); \
+	SPECIALIZED8_SINGLE_LINE_PRINTF(name, 7); \
+	SPECIALIZED8_SINGLE_LINE_PRINTF(name, 8); \
+	SPECIALIZED8_LINE_X_PRINTF(name);
+
+void fdisassemble(FILE *f, int ip, int *program, int level) {
+	fprintf(f, "#ifdef DEBUG\n");
+	print_indent(f, level);
+	fprintf(f, "printf(\"%%6d: \", %d);\n", ip);
+	print_indent(f, level);
+	switch(*program) {
+		SPECIALIZED8_PRINTF(INCR);
+		SPECIALIZED8_PRINTF(DECR);
+		SPECIALIZED8_PRINTF(LEFT);
+		SPECIALIZED8_PRINTF(RIGHT);
+		case INPUT: fprintf(f, "printf(\"INPUT\");\n"); break;
+		case OUTPUT: fprintf(f, "printf(\"OUTPUT\");\n"); break;
+		case JMPZ:
+			fprintf(f, "printf(\"JMPZ\\t\\t%%d\", %d);\n", *(program + 1));
+			break;
+		case JMPNZ:
+			fprintf(f, "printf(\"JMPNZ\\t\\t%%d\", %d);\n", *(program + 1));
+			break;
+		case RESET_CELL: fprintf(f, "printf(\"RESET_CELL\");\n"); break;
+		case END: fprintf(f, "printf(\"END\");\n"); break;
+		case START: fprintf(f, "printf(\"START\");\n"); break;
+	}
+	fprintf(f, "#endif\n");
+}
+
+#endif
+
+// returns the current cell pointer
+int transpile_rec(FILE *f, int *source, int **pgm, int startPointer,
+                  int level) {
 	int *program = *pgm;
+	// Pointer to the present cell, with respect to
+	// the start of the loop
+	int currentPointer = startPointer;
+	// Each index i of the array holds the total increment
+	// / decrement of the cell at pointer i with respect
+	// to the present loop
+	ChangeArray totalChange;
+	Change_array_init(&totalChange);
 	while(1) {
-		for(int i = 0; i < level - (*program == END || *program == JMPNZ);
-		    i++) {
-			fprintf(f, "\t");
+		// flush out the changes as required
+		switch(*program) {
+			// case INPUT:
+			case OUTPUT:
+			case JMPZ:
+			case JMPNZ:
+			case RESET_CELL:
+			case END:
+			case START:
+				dump_changes(f, &totalChange, currentPointer - startPointer,
+				             level);
+				print_indent(f, level - (*program == END || *program == JMPNZ));
+				startPointer = currentPointer;
+				break;
 		}
+#ifdef DEBUG
+		fdisassemble(f, program - source, program, level);
+#endif
 		switch(*program++) {
-			SPECIALIZED8_LINE(INCR, (*cell), +);
-			SPECIALIZED8_LINE(DECR, (*cell), -);
-			SPECIALIZED8_LINE(LEFT, cell, -);
-			SPECIALIZED8_LINE(RIGHT, cell, +);
-			case INPUT: fprintf(f, "*cell = getchar();\n"); break;
-			case OUTPUT: fprintf(f, "putchar(*cell);\n"); break;
+			SPECIALIZED8_LINE2(INCR, &totalChange, +);
+			SPECIALIZED8_LINE2(DECR, &totalChange, -);
+			SPECIALIZED8_LINE(LEFT, currentPointer, -);
+			SPECIALIZED8_LINE(RIGHT, currentPointer, +);
+			case INPUT:
+				print_indent(f, level);
+				fprintf(f, "cell[%d] = getchar();\n",
+				        currentPointer - startPointer);
+				break;
+			case OUTPUT: fprintf(f, "putchar(*cell);\n");
+#ifdef DEBUG
+				fprintf(f, "#ifdef DEBUG\n");
+				print_indent(f, level);
+				fprintf(f, "fflush(stdout);\n");
+				fprintf(f, "#endif\n");
+#endif
+				break;
 			case JMPZ:
 				program++; // ignore
 				fprintf(f, "while(*cell) {\n");
-				transpile_rec(f, &program, level + 1);
+				startPointer = currentPointer = transpile_rec(
+				    f, source, &program, currentPointer, level + 1);
 				fprintf(f, "}\n");
+				// reinit the array
+				Change_array_init(&totalChange);
 				break;
 			case JMPNZ:
 				program++; // ignore
 				*pgm = program;
-				return;
-			case RESET_CELL: fprintf(f, "*cell = 0;\n"); break;
-			case END: return;
-			case START: return;
+				return currentPointer;
+			case RESET_CELL:
+				// print_indent(f, level);
+				// fprintf(f, "cell[%d] = 0;\n", currentPointer - startPointer);
+				fprintf(f, "*cell = 0;\n");
+				break;
+			case END: return currentPointer;
+			case START: return currentPointer;
 		}
 	}
 }
 
-void transpile(const char *filename, Array *program) {
+void transpile(const char *filename, IntArray *program) {
 	// change all the dots to underscore
 	for(char *c = (char *)filename; *c; c++) {
 		if(*c == '.') {
@@ -322,7 +545,7 @@ void transpile(const char *filename, Array *program) {
 	fprintf(f, "\tuint8_t *cell = memory;\n");
 	fprintf(f, "\tclock_t start = clock();\n");
 	int *pgm = program->values;
-	transpile_rec(f, &pgm, 1);
+	transpile_rec(f, program->values, &pgm, 0, 1);
 	fprintf(f, "\tprintf(\"\\nElapsed: %%fs\\n\",(double)(clock() - "
 	           "start)/CLOCKS_PER_SEC);\n");
 	fprintf(f, "\treturn 0;\n");
@@ -347,16 +570,17 @@ int main(int argc, char *argv[]) {
 	fclose(f);
 	program[len] = 0;
 	// printf("%s\n", program);
-	Array *compiled = compile(program);
+	IntArray *compiled = compile(program);
 	free(program);
 	if(compiled == NULL) {
 		printf("Error occurred while compilation!\n");
 		return 2;
 	}
+	// disassemble_all(compiled);
 	transpile(argv[1], compiled);
 	clock_t start = clock();
 	execute(compiled);
 	printf("Elapsed: %fs\n", (double)(clock() - start) / CLOCKS_PER_SEC);
-	array_free(compiled);
+	int_array_free(compiled);
 	free(compiled);
 }
